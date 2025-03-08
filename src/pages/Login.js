97714388e06a7f38e959fba4abbc33659b2810ff
zainab-ajmal <zainab.ajmal68@gmail.com>
2025-03-08 19:47:19 +0500

    login and signup modified

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import UserAuth from '../components/UserAuth/UserAuth'
 import { useNavigate } from 'react-router-dom'
+import './Login.css'
+
+
+
 const Login = () => {
   const navigate=useNavigate();
   return (
@@ -13,18 +17,37 @@ const Login = () => {
         <label>Password</label>
         <input className='user-input' />
         <button style={{ backgroundColor: "transparent", border: "none" }} onClick={()=>{navigate('/forgot-password')}}>Forgot Password ?</button>
-        <button className='btn btn-dark my-3' style={{ width: "100%" }} onClick={()=>{navigate('/dashboard')}}>Continue</button>
-        <div className='fs-5 fw-semibold'>or</div>
-        <button className="btn my-2" style={{ border: "1px solid #bdbdbd", width: "100%", backgroundColor: "white", color: "#bdbdbd", fontSize: "14px" }} >SignUp with Google</button>
-        <button className="btn my-2" style={{ border: "1px solid #bdbdbd", width: "100%", backgroundColor: "white", color: "#bdbdbd", fontSize: "14px" }}>SignUp with Facebook</button>
-        <p>New Here?
-          <button style={{ backgroundColor: "transparent", border: "none" }} className='fw-semibold' onClick={()=>{navigate('/signup')}}>SIGN UP HERE</button>
+        <button className='btn btn-dark my-3' style={{ width: "100%" }}>Continue</button>
+        <div className="or-container">
+        <span className="line"></span>
+        <span className="fs-5 fw-semibold mx-2">or</span>
+        <span className="line"></span>
+        </div>
+
+        <button className="btn my-2 social-button" style={{ display:"flex", alignItems:"center",justifyContent:"center", border: "1px solid #bdbdbd", width: "100%", backgroundColor: "white", color: "#bdbdbd", fontSize: "14px", gap:"20px"}} >
+        <img src="/google-logo.png" alt="Google Logo" className="social-logo" />
+        Sign up with Google
+        </button>
+
+        <button className="btn my-2 social-button" style={{ display:"flex", alignItems:"center",justifyContent:"center", border: "1px solid #bdbdbd", width: "100%", backgroundColor: "white", color: "#bdbdbd", fontSize: "14px", gap:"20px"}} >
+        <img src="/fb-logo.png" alt="Facebook Logo" className="social-logo" />
+        Sign up with Facebook
+        </button>
+
+        <button className="btn my-2 social-button" style={{ display:"flex", alignItems:"center",justifyContent:"center", border: "1px solid #bdbdbd", width: "100%", backgroundColor: "white", color: "#bdbdbd", fontSize: "14px", gap:"20px"}} >
+        <img src="/apple-logo.jpg" alt="Apple Logo" className="social-logo" />
+        Sign up with Apple
+        </button>
+
+        <p style={{marginLeft:"90px",marginTop:"20px"}}>New Here?
+        <button style={{ backgroundColor: "transparent", border: "none", marginLeft: "5px", textDecoration: "underline"}} className='fw-semibold' onClick={()=>{navigate('/signup')}}>SIGN UP HERE</button>
         </p>
-
-
+       
       </div>
     </UserAuth>
   )
 }
+
+
 
 export default Login
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import UserAuth from '../components/UserAuth/UserAuth'
 import { useNavigate } from 'react-router-dom'
+import './Login.css'
 
 const SignUp = () => {
     const navigate=useNavigate();
@@ -16,15 +17,30 @@ const SignUp = () => {
                 <label>Password</label>
                 <input className='user-input' />
                 <button className='btn btn-dark my-3' style={{ width: "100%" }}>Get Started</button>
-                <div className='fs-5 fw-semibold'>or</div>
-                <button className="btn my-2" style={{ border: "1px solid #bdbdbd", width: "100%", backgroundColor: "white", color: "#bdbdbd", fontSize: "14px" }} >SignUp with Google</button>
-                <button className="btn my-2" style={{ border: "1px solid #bdbdbd", width: "100%", backgroundColor: "white", color: "#bdbdbd", fontSize: "14px" }}>SignUp with Facebook</button>
-                <p>Already have an account?
-                    <button style={{ backgroundColor: "transparent", border: "none" }} className='fw-semibold' onClick={()=>{navigate('/login')}}>LOGIN HERE</button>
+                <div className="or-container">
+                <span className="line"></span>
+                <span className="fs-5 fw-semibold mx-2">or</span>
+                <span className="line"></span>
+                </div>
+
+                <button className="btn my-2 social-button" style={{ display:"flex", alignItems:"center",justifyContent:"center", border: "1px solid #bdbdbd", width: "100%", backgroundColor: "white", color: "#bdbdbd", fontSize: "14px", gap:"20px"}} >
+                <img src="/google-logo.png" alt="Google Logo" className="social-logo" />
+                Log in with Google </button>
+
+                <button className="btn my-2 social-button" style={{ display:"flex", alignItems:"center",justifyContent:"center", border: "1px solid #bdbdbd", width: "100%", backgroundColor: "white", color: "#bdbdbd", fontSize: "14px", gap:"20px"}} >
+                <img src="/fb-logo.png" alt="Facebook Logo" className="social-logo" />
+                Log in with Facebook </button>
+
+                <button className="btn my-2 social-button" style={{ display:"flex", alignItems:"center",justifyContent:"center", border: "1px solid #bdbdbd", width: "100%", backgroundColor: "white", color: "#bdbdbd", fontSize: "14px", gap:"20px"}} >
+                <img src="/apple-logo.jpg" alt="Apple Logo" className="social-logo" />
+                Log in with Apple </button>
+
+                <p style={{marginLeft:"50px",marginTop:"10px"}}>Already have an account?
+                <button style={{ backgroundColor: "transparent", border: "none", marginLeft: "2px", textDecoration: "underline"}} className='fw-semibold' onClick={()=>{navigate('/signup')}}>LOGIN HERE</button>
                 </p>
 
 
-            </div>
+            </div> 
         </UserAuth>
     )
 }
