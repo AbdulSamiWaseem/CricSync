30dee5f7341ae12c0614c15bb6cf358e0a9cdef6
AbdulSamiWaseem <abdulsamiwaseem26@gmail.com>
2025-03-08 20:46:23 +0500

    updated version

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
   const navigate=useNavigate();
   return (
     <UserAuth>
-      <div className='d-flex flex-column m-3 p-3 align-items-start'>
+      <div className='d-flex flex-column p-3 align-items-start'>
         <div className='pb-1' style={{ fontSize: "12px" }}> Welcome Back</div>
         <div className='fs-4 fw-semibold ml-1 pb-3'>Login To Your Account</div>
         <label>Email</label>
@@ -17,7 +17,7 @@ const Login = () => {
         <label>Password</label>
         <input className='user-input' />
         <button style={{ backgroundColor: "transparent", border: "none" }} onClick={()=>{navigate('/forgot-password')}}>Forgot Password ?</button>
-        <button className='btn btn-dark my-3' style={{ width: "100%" }}>Continue</button>
+        <button className='btn btn-dark my-3' style={{ width: "100%" }} onClick={()=>{navigate('/dashboard')}}>Continue</button>
         <div className="or-container">
         <span className="line"></span>
         <span className="fs-5 fw-semibold mx-2">or</span>
